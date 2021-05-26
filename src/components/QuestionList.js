a1d52ae1e90208a67d050cd3b7ddfd4fb2944f8e
johnny <johnny1194@hotmail.com>
2021-05-26 14:17:34 -0400

    completed asignments

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function QuestionList() {
+
+  const [questions,setQuestions] = useState([]);
+
+  useEffect(()=> {
+    fetch("http://localhost:4000/questions")
+    .then(resp=>resp.json())
+    .then(data=>setQuestions(data))
+  },[])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
